d3b35d07d33b3f01ca6733ba4e20e8f6dacc5342
deepanshu <deepanshukumawat16@gmail.com>
2023-09-19 08:35:24 +0530

    se

--- node.js
+++ node.js
@@ -1,8 +1,10 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const cors = require("cors");
 const express = require("express");
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 server.get("/user", async (req, res) => {
   try {
@@ -17,26 +19,80 @@ server.get("/user", async (req, res) => {
 server.post("/user", async (req, res) => {
   const user = await prisma.user.create({
     data: {
-      title: req.body.title,
+      time: req.body.time,
       content: req.body.content,
+      image: req.body.image,
     },
   });
   res.send(user);
 });
 
-server.put("/user/:id", async (req, res) => {
-  const userId = parseInt(req.params.id);
+server.get("/admin", async (req, res) => {
+  const admin = await prisma.admin.findMany();
+  res.status(200).send(admin);
+});
+server.get("/movie", async (req, res) => {
+  const movie = await prisma.movie.findMany();
+  res.status(200).send(movie);
+});
+server.post("/admin", async (req, res) => {
+  console.log(req.body);
+  const admin = await prisma.admin.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      password: req.body.password,
+    },
+  });
+  res.send(admin);
+});
+
+server.post("/movieadd", async (req, res) => {
+  console.log(req.body);
+  const movie = await prisma.movie.create({
+    data: req.body,
+  });
+  res.send(movie);
+});
+
+server.delete("/moviedelete/:id", async (req, res) => {
+  const movieId = parseInt(req.params.id);
   try {
-    const user = await prisma.user.update({
-      where: { id: userId },
-      data: {
-        title: req.body.title,
-        content: req.body.content,
-      },
+    await prisma.movie.delete({
+      where: { id: movieId },
     });
-    res.send(user);
+    res.status(204).send();
   } catch (error) {
-    res.status(404).send("not");
+    res.status(404).send(" not ");
+  }
+});
+
+server.delete("/admin/:id", async (req, res) => {
+  const adminId = parseInt(req.params.id);
+  try {
+    await prisma.admin.delete({
+      where: { id: adminId },
+    });
+    res.status(204).send();
+  } catch (error) {
+    res.status(404).send(" not ");
+  }
+});
+// use for bulk data
+server.post("/movie", async (req, res) => {
+  try {
+    const movieArray = req.body;
+    const moviesCreated = await prisma.movie.createMany({
+      data: movieArray.map((movie) => ({
+        name: movie.name,
+        description: movie.description,
+        image: movie.image,
+      })),
+    });
+    res.status(204).send(moviesCreated);
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).send(e.message);
   }
 });
 
@@ -62,7 +118,7 @@ server.get("/plan", async (req, res) => {
     res.status(500).json({ error: "not" });
   }
 });
-
+// insert
 server.post("/plan", async (req, res) => {
   const user = await prisma.user.create({
     data: {
@@ -72,7 +128,7 @@ server.post("/plan", async (req, res) => {
   });
   res.send(user);
 });
-
+// update
 server.put("/plan/:id", async (req, res) => {
   const userId = parseInt(req.params.id);
   try {
@@ -101,6 +157,6 @@ server.delete("/plan/:id", async (req, res) => {
   }
 });
 
-server.listen(3000, () => {
-  console.log("server started at 3000");
+server.listen(4000, () => {
+  console.log("server started at 4000");
 });
